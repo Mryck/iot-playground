0247ad2ab40609ca9b548080b27ca145c3e7f682
Mryck <emeric.bombenger@me.com>
2019-10-14 21:33:47 +0200

    Inital front mockup

---
+++ front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <section class="hero is-primary">
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div id="navbarMenuHeroB" class="navbar-menu">
+              <div class="navbar-end">
+                <span class="navbar-item">
+                  <a class="button is-primary is-inverted">
+                    <span>Configuration</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+    <br />
+    <div class="container">
+      <Dashboard></Dashboard>
+    </div>
+  </div>
+</template>
+
+<script>
+import Dashboard from "./components/Dashboard.vue";
+
+export default {
+  name: "app",
+  components: {
+    Dashboard
+  }
+};
+</script>
+
---
+++ front/src/components/Dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="columns is-multiline">
+      <div class="column is-one-quarter" v-for="sensor in sensors" :key="sensor.topic">
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <p class="title is-5">{{ sensor.topic }}</p>
+              <p class="is-size-1 has-text-centered">{{ sensor.payload }}</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+//import io from "socket.io-client";
+
+export default {
+  data() {
+    return {
+      sensors: [
+        { topic: "home", payload: "23" },
+        { topic: "bedroom", payload: "20" },
+        { topic: "outside", payload: "19" }
+      ]
+      //socket: io("http://localhost:5000/")
+    };
+  },
+  mounted() {
+    // this.socket.on("mqtt_message", data => {
+    //   this.sensors.push(data);
+    // });
+  }
+};
+</script>
