65574870223fd6cfdeb5dd3ddbad58db032b1d1a
Mryck <emeric.bombenger@me.com>
2019-10-29 13:09:24 +0100

    autoformat and linting

--- back/run.py
+++ back/run.py
@@ -9,34 +9,35 @@ eventlet.monkey_patch()
 app = Flask(__name__)
 
 # Basic configuration for flask_mqtt
-app.config["MQTT_BROKER_URL"] = "mosquitto"
-app.config["MQTT_BROKER_PORT"] = 1883
-app.config["MQTT_CLIENT_ID"] = "flask_mqtt"
-app.config["MQTT_KEEPALIVE"] = 5
-app.config["MQTT_TLS_ENABLED"] = False
-app.config["SECRET_KEY"] = "secret!"
+app.config['MQTT_BROKER_URL'] = 'mosquitto'
+app.config['MQTT_BROKER_PORT'] = 1883
+app.config['MQTT_CLIENT_ID'] = 'flask_mqtt'
+app.config['MQTT_KEEPALIVE'] = 5
+app.config['MQTT_TLS_ENABLED'] = False
+app.config['SECRET_KEY'] = 'secret!'
 
 mqtt = Mqtt(app)
 
 # Setup socket io to allow cors origni from anywhere and print log
-socketio = SocketIO(app, cors_allowed_origins="*", engineio_logger=True)
+socketio = SocketIO(app, cors_allowed_origins='*', engineio_logger=True)
 
 
-@socketio.on("subscribe")
+@socketio.on('subscribe')
 def handle_subscribe(json_str):
     data = json.loads(json_str)
-    mqtt.subscribe(data["topic"], data["qos"])
+    mqtt.subscribe(data['topic'], data['qos'])
 
 
-@socketio.on("unsubscribe_all")
+@socketio.on('unsubscribe_all')
 def handle_unsubscribe_all():
     mqtt.unsubscribe_all()
 
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
-    data = dict(topic=message.topic, payload=message.payload.decode(), qos=message.qos)
-    socketio.emit("mqtt_message", data=data)
+    data = dict(topic=message.topic,
+                payload=message.payload.decode(), qos=message.qos)
+    socketio.emit('mqtt_message', data=data)
 
 
 @mqtt.on_log()
@@ -44,5 +45,5 @@ def handle_logging(client, userdata, level, buf):
     print(level, buf)
 
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0')
